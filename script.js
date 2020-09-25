e2a3fcb240c23b1cc4f64163fd4002f3c5ec02f8
Erick <a326820@uach.mx>
2020-09-25 02:35:36 -0600

    fechita

---
+++ script.js
@@ -0,0 +1,5 @@
+var fecha = () => { 
+    document.getElementById('demo').innerHTML = Date();
+    document.getElementById('foto').src = "foto.jpg" ;
+    document.getElementById('foto').style="width: 230px; height: 200px;"
+}
